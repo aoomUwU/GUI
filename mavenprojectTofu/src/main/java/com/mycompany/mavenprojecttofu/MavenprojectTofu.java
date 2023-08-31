21c12ae93cb7db57936b3e278585fe1720e20ead
aoomUwU <119989552+aoomUwU@users.noreply.github.com>
2023-08-31 12:24:08 +0700

    Add files via upload

---
+++ mavenprojectTofu/src/main/java/com/mycompany/mavenprojecttofu/MavenprojectTofu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenprojecttofu;
+
+/**
+ *
+ * @author CLASSROOM
+ */
+public class MavenprojectTofu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
